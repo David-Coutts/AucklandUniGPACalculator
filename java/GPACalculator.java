e42e02379dfdf0206a615bfefff2d4bcef9a87ea
bandicoutts <d.coutts@gmail.com>
2019-02-25 23:02:54 +1300

    undoAddition() and error correction
    Implemented undoAddition(), which will undo the addition of the last added grade in case the student made a mistake adding a particular grade. Also updated .getText() in addGradeButton's actionListener so that it will make the input into an upper case character, in case the student types "a" instead of "A".

--- java/GPACalculator.java
+++ java/GPACalculator.java
@@ -4,8 +4,11 @@ import java.util.*;
 import java.awt.event.*;
 
 class grade{
-    private static int gradeTotal = 0;
-    private static int subjectCount = 0;
+
+    private static int gradeTotal;
+    private static int subjectCount;
+    private static int previousGradeTotal;
+    private static int previousSubjectCount;
 
     public static int getValue() {
         if (subjectCount == 0){
@@ -20,44 +23,78 @@ class grade{
         subjectCount = 0;
     }
 
+    public static void undoAddition(){
+        // this will allow users to step back one grade addition, just in case they've made a mistake
+        if (previousSubjectCount != 0){
+            gradeTotal = previousGradeTotal;
+            subjectCount = previousSubjectCount;
+        }
+    }
+
     public static void setValue(String inputVariable){
         if (inputVariable.equals("A+")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 9;
             subjectCount += 1;
         } else if (inputVariable.equals("A")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 8;
             subjectCount += 1;
         } else if (inputVariable.equals("A-")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 7;
             subjectCount += 1;
         } else if (inputVariable.equals("B+")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 6;
             subjectCount += 1;
         } else if (inputVariable.equals("B")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 5;
             subjectCount += 1;
         } else if (inputVariable.equals("B-")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 4;
             subjectCount += 1;
         } else if (inputVariable.equals("C+")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 3;
             subjectCount += 1;
         } else if (inputVariable.equals("C")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 2;
             subjectCount += 1;
         } else if (inputVariable.equals("C-")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 1;
             subjectCount += 1;
         } else if (inputVariable.equals("D+")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 0;
             subjectCount += 1;
         } else if (inputVariable.equals("D")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 0;
             subjectCount += 1;
         } else if (inputVariable.equals("D-")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 0;
             subjectCount += 1;
         } else if (inputVariable.equals("F")){
+            previousGradeTotal = gradeTotal;
+            previousSubjectCount = subjectCount;
             gradeTotal += 0;
             subjectCount += 1;
         }
@@ -76,12 +113,14 @@ class GPACalculator {
         JLabel promptLabel = new JLabel("Enter your grade (e.g. A+, A, A-, etc.):");
         JTextField textField = new JTextField(10);
         JButton addGradeButton = new JButton("Submit");
+        JButton undoGradeButton = new JButton("Undo");
         JButton resetButton = new JButton("Reset");
         JTextArea textArea = new JTextArea("Your GPA is " + grade.getValue());
         panel.add(promptLabel);
         panel.add(promptLabel);
         panel.add(textField);
         panel.add(addGradeButton);
+        panel.add(undoGradeButton);
         panel.add(resetButton);
         panel.add(textArea);
 
@@ -90,11 +129,19 @@ class GPACalculator {
         {
             public void actionPerformed(ActionEvent e)
             {
-                String variable = textField.getText();
+                String variable = textField.getText().toUpperCase();
                 grade.setValue(variable);
                 textArea.setText("Your GPA is " + grade.getValue());
             }
         });
+
+        undoGradeButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                grade.undoAddition();
+                textArea.setText("Your GPA is " + grade.getValue());
+            }
+                                          });
+
         resetButton.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
@@ -103,6 +150,7 @@ class GPACalculator {
                 textArea.setText("Your GPA is " + grade.getValue());
             }
         });
+
 
         //setting layout
         frame.getContentPane().add(BorderLayout.CENTER, panel);
